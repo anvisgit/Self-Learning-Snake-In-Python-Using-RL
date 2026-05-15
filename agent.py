645701370bb14f1837805ba7d4a712ea31bf9ccd
anvisgit <anvi.wadhwa2024@vitstudent.ac.in>
2026-05-15 19:08:39 +0000

    agent final

--- agent.py
+++ agent.py
@@ -14,17 +14,72 @@ class Agent:
         self.epsilon=0 #randomness
         self.gamma=0 #disc rate
         self.memory= deque(maxlen=MAX_MEMORY)#popleft
+        self.model=None
+
 
     def get_state(self,game):
-        pass
+        head= game.snake[0]
+        point_l=point(head.x -20, head.y)
+        point_r=point(head.x+20, head.y)
+        point_u=point(head.x,head.y-20)
+        point_d=point(head.x,head.y+20)
+
+        dir_l=game.direction==Direction.LEFT
+        dir_r=game.direction==Direction.RIGHT
+        dir_u=game.direction==Direction.UP
+        dir_d=game.direction==Direction.DOWN
+
+        state=[
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)) , 
+
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)) , 
+
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+
+            dir_l, dir_r, dir_u, dir_d, 
+
+            game.food.x<game.head.x, 
+            game.food.x>game.head.x,
+            game.food.y<game.head.y, 
+            game.food.y>game.head.y]
+
+        return np.array(state, dtype=int)
+
+
     def remember(self, state, action, reward,next_state, done ):
-        pass
+        self.memory.append(state, action, reward,next_state, done)
     def train_long_memory(self):
-        pass
+        if len(self.memory)>BATCH_SIZE:
+            mini_sample=random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample=self.memory
+        states, actions, rewards, next_states, dones=zip(*mini_sample)    
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+
+
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
     def get_action(self,state):
-        pass
+        self.epsilon=80-self.n_games
+        final_move=[0,0,0]
+        if random.randint(0,200)<self.epsilon:
+            move=random.randint(0,2)
+            final_move[move]=1
+        else:
+            state0=torch.tensor(state, dtype=torch.float)
+            prediction =self.model.predict(state0)
+            move=torch.argmax(prediction).item()
+            final_move=1
+        return final_move
 def train():
     plot_scores=[]
     plot_mean_scores=[]
